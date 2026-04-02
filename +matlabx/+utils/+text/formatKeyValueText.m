7a3cbc204dee80362e12b58a1e1e6b23c7da4e3c
will-f-dean-36 <will.f.dean.36@gmail.com>
2026-04-02 12:25:57 -0500

    Comments

--- +matlabx/+utils/+text/formatKeyValueText.m
+++ +matlabx/+utils/+text/formatKeyValueText.m
@@ -1,10 +1,11 @@
 function txt = formatKeyValueText(keys, values)
-
+%FORMATKEYVALUETEXT Formats two cell arrays of char vectors as a text table for display
+    % maximum length of the left column of text
     maxLen = max(cellfun(@numel, keys));
-
+    % add each line of the table to separate cell
     lines = cellfun(@(k,v) ...
         sprintf('%-*s  %s', maxLen, k, v), ...
         keys, values, 'UniformOutput', false);
-
+    % join all lines with newline
     txt = strjoin(lines, newline);
 end
