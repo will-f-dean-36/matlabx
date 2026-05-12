8dd2c9e6948d9914cc1f3348f2b1dc4cdb502a33
will-f-dean-36 <will.f.dean.36@gmail.com>
2026-05-12 11:34:04 -0500

    Changed how resizing is handled, compatibility updates for Image5D

--- +matlabx/+app/quickshow.m
+++ +matlabx/+app/quickshow.m
@@ -1,8 +1,12 @@
-function ax = quickshow(I,cmap,opts)
+function ax = quickshow(I,opts)
     arguments
-        I (:,:,:)
-        cmap (256,3) double = gray
-        opts.Title (1,:) char = 'Untitled'
+        I
+        opts.Colormap (256,3) double
+        opts.Title (1,:) char = 'Viewer'
+    end
+
+    if ~isfield(opts,'Colormap') || isempty(opts.Colormap)
+        opts.Colormap = gray(256);
     end
 
     f = uifigure(...
@@ -12,16 +16,126 @@ function ax = quickshow(I,cmap,opts)
         "AutoResizeChildren",   "off",...
         "Name",                 opts.Title);
 
-    ax = matlabx.ui.widgets.ImageAxes(f,...
-        "ToolBelt",             {'Zoom', 'Colorbar', 'ChooseColormap'},...
-        "CData",                I,...
-        "Units",                "normalized",...
-        "Position",             [0 0 1 1],...
-        "Colormap",             cmap,...
-        "Name",                 "Viewer");
+    if isa(I,"matlabx.image.Image5D")
+        ax = matlabx.ui.widgets.ImageAxes(f,...
+            "ToolBelt",     {'Zoom', 'Colorbar', 'ChooseColormap', 'Pick'},...
+            "ImageData",    I,...
+            "Units",        "normalized",...
+            "Position",     [0 0 1 1],...
+            "Name",         "Viewer");
+    else
+        ax = matlabx.ui.widgets.ImageAxes(f,...
+            "ToolBelt",     {'Zoom', 'Colorbar', 'ChooseColormap'},...
+            "CData",        I,...
+            "Units",        "normalized",...
+            "Position",     [0 0 1 1],...
+            "Colormap",     opts.Colormap,...
+            "Name",         "Viewer");
+    end
+
+    cal = matlabx.ui.calibration.getCalibration();
+    panelTopChromePx = cal.uipanelTopChromeHeightPx(ax.FontSize,"FontUnits","pixels");
+    f.Position(3) = f.Position(4) - panelTopChromePx;
+
 
     movegui(f,"center")
 
     f.Visible = "on"; % Make the figure visible
 
 end
+
+
+
+
+
+
+
+
+
+
+
+% function ax = quickshow(I,opts)
+%     arguments
+%         I
+%         opts.Colormap (256,3) double
+%         opts.Title (1,:) char = 'Viewer'
+%     end
+% 
+%     if ~isfield(opts,'Colormap') || isempty(opts.Colormap)
+%         opts.Colormap = gray(256);
+%     end
+% 
+%     f = uifigure(...
+%         "WindowStyle",          "alwaysontop",...
+%         "Position",             [0 0 500 500],...
+%         "Visible",              "off",...
+%         "AutoResizeChildren",   "off",...
+%         "Name",                 opts.Title,...
+%         "SizeChangedFcn",       @(o,e) updateOnResize());
+% 
+% 
+%     mainGrid = uigridlayout(f,[1,1],...
+%         "ColumnWidth",{'fit'},...
+%         "RowHeight",{'fit'},...
+%         "Padding",[0 0 0 0]);
+% 
+%     panelGrid = uigridlayout(mainGrid,[1,1],...
+%         "RowHeight",{500},...
+%         "ColumnWidth",{500},...
+%         "Padding",[0 0 0 0]);
+%     panelGrid.Layout.Row = 1;
+%     panelGrid.Layout.Column = 1;
+% 
+%     p = uipanel("Parent",panelGrid,...
+%         "Title","Test",...
+%         "FontSize",10,...
+%         "AutoResizeChildren","off");
+% 
+% 
+%     aspectRatio = I.SizeX/I.SizeY;
+%     cal = matlabx.ui.calibration.getCalibration();
+%     panelTop = cal.uipanelTopChromeHeightPx(10);
+% 
+%     if isa(I,"matlabx.image.Image5D")
+%         f.InnerPosition(4) = f.InnerPosition(3)/aspectRatio+panelTop;
+% 
+%         ax = matlabx.ui.widgets.ImageAxes(p,...
+%             "ToolBelt",     {'Zoom', 'Colorbar', 'ChooseColormap', 'Pick'},...
+%             "ImageData",    I,...
+%             "Units",        "normalized",...
+%             "Position",     [0 0 1 1],...
+%             "Name",         "Viewer");
+%     else
+%         ax = matlabx.ui.widgets.ImageAxes(f,...
+%             "ToolBelt",     {'Zoom', 'Colorbar', 'ChooseColormap'},...
+%             "CData",        I,...
+%             "Units",        "normalized",...
+%             "Position",     [0 0 1 1],...
+%             "Colormap",     opts.Colormap,...
+%             "Name",         "Viewer");
+%     end
+% 
+%     movegui(f,"center")
+% 
+%     f.Visible = "on"; % Make the figure visible
+% 
+% 
+%     function updateOnResize()
+% 
+%         iPos = f.InnerPosition;
+%         W = iPos(3);
+%         H = iPos(4);
+% 
+%         if W > H
+%             panelGrid.RowHeight{1} = H;
+%             panelGrid.ColumnWidth{1} = H-panelTop;
+%         elseif H > W
+%             panelGrid.ColumnWidth{1} = W;
+%             panelGrid.RowHeight{1} = W+panelTop;
+%         end
+% 
+%     end
+% 
+% end
+
+
