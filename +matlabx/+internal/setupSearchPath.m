b66ca49db7d646c3ea54953a3e1d8ea63e639ce4
will-f-dean-36 <will.f.dean.36@gmail.com>
2026-06-24 00:36:46 -0500

    Add project root to the search path in the setupSearchPath helper

--- +matlabx/+internal/setupSearchPath.m
+++ +matlabx/+internal/setupSearchPath.m
@@ -1,6 +1,9 @@
 function setupSearchPath()
 %SETUPSEARCHPATH Adds necessary folder to MATLAB search path
 
+% add root folder to MATLAB search path
+addpath(matlabx.internal.Paths.root());
+
 % add external libraries to MATLAB search path
 addpath(genpath(matlabx.internal.Paths.external()));
 savepath();
