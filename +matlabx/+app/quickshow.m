02ff378295a026a99e94b6bfa56b490cc845ed69
will-f-dean-36 <will.f.dean.36@gmail.com>
2026-06-27 02:14:37 -0500

    Add a few input arguments to make quickshow more useful/flexible

--- +matlabx/+app/quickshow.m
+++ +matlabx/+app/quickshow.m
@@ -1,31 +1,58 @@
-function ax = quickshow(I,opts)
+function [ax,fig] = quickshow(I,opts)
     arguments
         I
         opts.Colormap (256,3) double
         opts.Title (1,:) char = 'Viewer'
+        opts.Tools (1,:) cell {mustBeMember(opts.Tools,{'Zoom','Colorbar','ChooseColormap','Pick','DrawRectangle'})} ...
+            = matlabx.ui.widgets.ImageAxes.getDefaultTools()
+        opts.WindowStyle (1,:) char {mustBeMember(opts.WindowStyle,{'normal','alwaysontop'})} = 'alwaysontop'
+        opts.Visible (1,1) matlab.lang.OnOffSwitchState = "on"
+        opts.Size (1,1) double = NaN
+        opts.Units (1,:) char = ''
     end
 
     if ~isfield(opts,'Colormap') || isempty(opts.Colormap)
         opts.Colormap = gray(256);
     end
 
-    f = uifigure(...
-        "WindowStyle",          "alwaysontop",...
-        "Position",             [0 0 500 500],...
+    if isnan(opts.Size) && isempty(opts.Units)
+        opts.Size = 500;
+        opts.Units = 'pixels';
+    elseif isnan(opts.Size) || isempty(opts.Units)
+        error('matlabx:app:quickshow:InvalidSizeOrUnits',...
+            'To set figure Size or Units, you must provide both as input arguments');
+    end
+
+    % get ui calibration helper
+    cal = matlabx.ui.calibration.getCalibration();
+
+    % convert size to pixels
+    switch opts.Units
+        case "inches"
+            opts.Size = opts.Size * cal.PixelsPerInch;
+        case "points"
+            opts.Size = opts.Size * cal.PixelsPerPoint;
+    end
+    
+
+    fig = uifigure(...
+        "WindowStyle",          opts.WindowStyle,...
+        "Position",             [0 0 opts.Size opts.Size],...
+        "Units",                "pixels",...
         "Visible",              "off",...
         "AutoResizeChildren",   "off",...
         "Name",                 opts.Title);
 
     if isa(I,"matlabx.image.Image5D")
-        ax = matlabx.ui.widgets.ImageAxes(f,...
-            "ToolBelt",     {'Zoom', 'Colorbar', 'ChooseColormap', 'Pick'},...
+        ax = matlabx.ui.widgets.ImageAxes(fig,...
+            "ToolBelt",     opts.Tools,...
             "ImageData",    I,...
             "Units",        "normalized",...
             "Position",     [0 0 1 1],...
             "Name",         "Viewer");
     else
-        ax = matlabx.ui.widgets.ImageAxes(f,...
-            "ToolBelt",     {'Zoom', 'Colorbar', 'ChooseColormap'},...
+        ax = matlabx.ui.widgets.ImageAxes(fig,...
+            "ToolBelt",     opts.Tools,...
             "CData",        I,...
             "Units",        "normalized",...
             "Position",     [0 0 1 1],...
@@ -33,109 +60,13 @@ function ax = quickshow(I,opts)
             "Name",         "Viewer");
     end
 
-    cal = matlabx.ui.calibration.getCalibration();
+    % cal = matlabx.ui.calibration.getCalibration();
     panelTopChromePx = cal.uipanelTopChromeHeightPx(ax.FontSize,"FontUnits","pixels");
-    f.Position(3) = f.Position(4) - panelTopChromePx;
+    fig.Position(4) = fig.Position(3) + panelTopChromePx;
 
 
-    movegui(f,"center")
+    movegui(fig,"center")
 
-    f.Visible = "on"; % Make the figure visible
+    fig.Visible = opts.Visible;
 
 end
-
-
-
-
-
-
-
-
-
-
-
-% function ax = quickshow(I,opts)
-%     arguments
-%         I
-%         opts.Colormap (256,3) double
-%         opts.Title (1,:) char = 'Viewer'
-%     end
-% 
-%     if ~isfield(opts,'Colormap') || isempty(opts.Colormap)
-%         opts.Colormap = gray(256);
-%     end
-% 
-%     f = uifigure(...
-%         "WindowStyle",          "alwaysontop",...
-%         "Position",             [0 0 500 500],...
-%         "Visible",              "off",...
-%         "AutoResizeChildren",   "off",...
-%         "Name",                 opts.Title,...
-%         "SizeChangedFcn",       @(o,e) updateOnResize());
-% 
-% 
-%     mainGrid = uigridlayout(f,[1,1],...
-%         "ColumnWidth",{'fit'},...
-%         "RowHeight",{'fit'},...
-%         "Padding",[0 0 0 0]);
-% 
-%     panelGrid = uigridlayout(mainGrid,[1,1],...
-%         "RowHeight",{500},...
-%         "ColumnWidth",{500},...
-%         "Padding",[0 0 0 0]);
-%     panelGrid.Layout.Row = 1;
-%     panelGrid.Layout.Column = 1;
-% 
-%     p = uipanel("Parent",panelGrid,...
-%         "Title","Test",...
-%         "FontSize",10,...
-%         "AutoResizeChildren","off");
-% 
-% 
-%     aspectRatio = I.SizeX/I.SizeY;
-%     cal = matlabx.ui.calibration.getCalibration();
-%     panelTop = cal.uipanelTopChromeHeightPx(10);
-% 
-%     if isa(I,"matlabx.image.Image5D")
-%         f.InnerPosition(4) = f.InnerPosition(3)/aspectRatio+panelTop;
-% 
-%         ax = matlabx.ui.widgets.ImageAxes(p,...
-%             "ToolBelt",     {'Zoom', 'Colorbar', 'ChooseColormap', 'Pick'},...
-%             "ImageData",    I,...
-%             "Units",        "normalized",...
-%             "Position",     [0 0 1 1],...
-%             "Name",         "Viewer");
-%     else
-%         ax = matlabx.ui.widgets.ImageAxes(f,...
-%             "ToolBelt",     {'Zoom', 'Colorbar', 'ChooseColormap'},...
-%             "CData",        I,...
-%             "Units",        "normalized",...
-%             "Position",     [0 0 1 1],...
-%             "Colormap",     opts.Colormap,...
-%             "Name",         "Viewer");
-%     end
-% 
-%     movegui(f,"center")
-% 
-%     f.Visible = "on"; % Make the figure visible
-% 
-% 
-%     function updateOnResize()
-% 
-%         iPos = f.InnerPosition;
-%         W = iPos(3);
-%         H = iPos(4);
-% 
-%         if W > H
-%             panelGrid.RowHeight{1} = H;
-%             panelGrid.ColumnWidth{1} = H-panelTop;
-%         elseif H > W
-%             panelGrid.ColumnWidth{1} = W;
-%             panelGrid.RowHeight{1} = W+panelTop;
-%         end
-% 
-%     end
-% 
-% end
-
-
