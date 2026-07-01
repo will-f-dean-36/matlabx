ee821c320522abad75fb953e0caa30a2b3bd1fee
will-f-dean-36 <will.f.dean.36@gmail.com>
2026-07-01 11:53:38 -0500

    Edit toRGB and fromRGB to use colornames()

--- +matlabx/+colors/+names/fromRGB.m
+++ +matlabx/+colors/+names/fromRGB.m
@@ -1,44 +1,52 @@
-function name = fromRGB(rgb)
-%FROMRGB  Converts an RGB triplet to a color name
+function [name, matchedRGB] = fromRGB(rgb, options)
+%FROMRGB  Converts an RGB triplet to the closest color name
 %
-%   name = FROMRGB(rgb) returns the color name corresponding 
-%   to the specified 1x3 RGB vector (values in [0,1]).
+%   name = FROMRGB(rgb) returns the closest matching color name from the
+%   CSS color palette for the specified 1x3 RGB vector.
 %
-%   Supported colors:
-%     'red', 'green', 'blue', 'cyan', 'magenta', 'yellow', 'white', 'black'
+%   [name, matchedRGB] = FROMRGB(rgb) also returns the RGB triplet
+%   associated with the matched color name.
+%
+%   [...] = FROMRGB(rgb, Palette=palette) specifies the color-name palette
+%   used for matching. The default palette is "CSS".
 %
 %   Input
-%     rgb  : 1x3 RGB triplet in [0,1]
+%     rgb         : 1x3 numeric RGB triplet with values in [0,1]
+%
+%   Name-Value Input
+%     Palette     : String scalar specifying a palette supported by
+%                   COLORNAMES. The default is "CSS".
 %
 %   Output
-%     name : character vector or string scalar specifying the color name
+%     name        : String scalar containing the closest color name
+%     matchedRGB  : 1x3 RGB triplet associated with the matched color name
 %
 %   Example
-%     rgb = fromRGB([1 0 1]);   % returns "magenta"
+%     [name, matchedRGB] = fromRGB([0.25 0.48 0.72])
 %
-%   See also COLORGRADIENT, TORGB
+%     [name, matchedRGB] = fromRGB([1 0 1], Palette="MATLAB")
+%
+%   See also COLORNAMES, TORGB
+
+    arguments
+        rgb (1,3) {mustBeNumeric, mustBeReal, mustBeFinite, ...
+            mustBeGreaterThanOrEqual(rgb,0), ...
+            mustBeLessThanOrEqual(rgb,1)}
+        options.Palette (1,1) string = "CSS"
+    end
 
     rgb = double(rgb);
 
-    if isequal(rgb, [1 0 0])
-        name = "red";
-    elseif isequal(rgb, [0 1 0])
-        name = "green";
-    elseif isequal(rgb, [0 0 1])
-        name = "blue";
-    elseif isequal(rgb, [0 1 1])
-        name = "cyan";
-    elseif isequal(rgb, [1 0 1])
-        name = "magenta";
-    elseif isequal(rgb, [1 1 0])
-        name = "yellow";
-    elseif isequal(rgb, [1 1 1])
-        name = "white";
-    elseif isequal(rgb, [0 0 0])
-        name = "black";
-    else
-        error('matlabx:colors:names:fromRGB:UnknownRGBTriplet', ...
-            'Unknown RGB triplet %s. Supported colors: red, green, blue, cyan, magenta, yellow, white, black.', mat2str(rgb));
+    try
+        [name, matchedRGB] = colornames(options.Palette, rgb);
+    catch ME
+        cause = MException( ...
+            "matlabx:colors:names:fromRGB:ColorLookupFailed", ...
+            'Unable to match RGB triplet %s using the "%s" palette.', ...
+            mat2str(rgb), options.Palette);
+
+        cause = addCause(cause, ME);
+        throwAsCaller(cause);
     end
 
 end
--- +matlabx/+colors/+names/toRGB.m
+++ +matlabx/+colors/+names/toRGB.m
@@ -1,45 +1,45 @@
-function rgb = toRGB(name)
+function rgb = toRGB(name, options)
 %TORGB  Converts a color name to an RGB triplet
 %
-%   rgb = TORGB(name) returns the 1x3 RGB vector (values in [0,1])
-%   corresponding to the specified color name. The lookup is case-
-%   insensitive.
+%   rgb = TORGB(name) returns the RGB triplet associated with the specified
+%   color name in the CSS color palette. Color-name matching is
+%   case-insensitive.
 %
-%   Supported colors:
-%     'red', 'green', 'blue', 'cyan', 'magenta', 'yellow', 'white', 'black'
+%   rgb = TORGB(name, Palette=palette) specifies the color-name palette
+%   used for lookup. The default palette is "CSS".
 %
 %   Input
-%     name : character vector or string scalar specifying the color name
+%     name      : Character vector or string scalar specifying a color name
+%
+%   Name-Value Input
+%     Palette   : String scalar specifying a palette supported by
+%                 COLORNAMES. The default is "CSS".
 %
 %   Output
-%     rgb  : 1x3 RGB triplet in [0,1]
+%     rgb       : 1x3 RGB triplet with values in [0,1]
 %
 %   Example
-%     rgb = toRGB('magenta');   % returns [1 0 1]
+%     rgb = toRGB("magenta")
 %
-%   See also COLORGRADIENT, FROMRGB
+%     rgb = toRGB("cyan", Palette="MATLAB")
+%
+%   See also COLORNAMES, FROMRGB
 
-    name = lower(string(name));
-
-    switch name
-        case "red"
-            rgb = [1 0 0];
-        case "green"
-            rgb = [0 1 0];
-        case "blue"
-            rgb = [0 0 1];
-        case "cyan"
-            rgb = [0 1 1];
-        case "magenta"
-            rgb = [1 0 1];
-        case "yellow"
-            rgb = [1 1 0];
-        case "white"
-            rgb = [1 1 1];
-        case "black"
-            rgb = [0 0 0];
-        otherwise
-            error('matlabx:colors:names:toRGB:UnknownColorName', ...
-                  'Unknown color name "%s". Supported colors: red, green, blue, cyan, magenta, yellow, white, black.', name);
+    arguments
+        name (1,1) string
+        options.Palette (1,1) string = "CSS"
     end
+
+    try
+        [~, rgb] = colornames(options.Palette, name);
+    catch ME
+        cause = MException( ...
+            "matlabx:colors:names:toRGB:ColorLookupFailed", ...
+            'Unable to find color name "%s" in the "%s" palette.', ...
+            name, options.Palette);
+
+        cause = addCause(cause, ME);
+        throwAsCaller(cause);
+    end
+
 end
